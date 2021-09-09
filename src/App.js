596851f4d3a0a51fc7ce3680a57696ac7000f32b
LeeSeohyun <mlsh1112@ajou.ac.kr>
2021-09-09 19:20:48 +0900

    [craete] Add Menu component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import virspitIcon from './assets/ViRSPiT.png'
 function App() {
   return (
     <div className="App">
-      
+      <header className="app_header">
+        <div className="header-logo-txt">
+          <img className="header_iconImg" src={virspitIcon} width="200" alt='virspit icon'/>
+          <div>ADMIN PAGE</div>
+        </div>
+        <div className="header_bar"></div>
+      </header>
+
+      <section >
+        
+      </section>
     </div>
   );
 }

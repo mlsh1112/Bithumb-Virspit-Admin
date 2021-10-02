2a64bd2ba3e47247c5eeb255bbcff91bfde417c0
LeeSeohyun <mlsh1112@ajou.ac.kr>
2021-10-02 21:08:22 +0900

    [update-feat] connect Sport api

--- src/api/API.js
+++ src/api/API.js
@@ -18,7 +18,10 @@ export const getproducts = () => axios.get(PRODUCT_PORT+'/product')
 export const searchproduct = () => axios.get()
 
 export const getplayers = () => axios.get(PRODUCT_PORT+'/team-player')
-export const getplayer = (id) => axios.get(PRODUCT_PORT+'/team-player/'+id)
+export const getplayer = (id) => axios.get(PRODUCT_PORT+`/team-player/${id}`)
 
 export const getsports = () => axios.get(PRODUCT_PORT+'/sports')
-export const getsport = (id) => axios.get(PRODUCT_PORT+'/sports'+id)
+export const getsport = (id) => axios.get(PRODUCT_PORT+`/sports/${id}`)
+export const editsport = (id,param) => axios.put(PRODUCT_PORT+`/sports/${id}`,param)
+export const updatesport = (param) => axios.post(PRODUCT_PORT+'/sports',param)
+export const deletesport = (id) => axios.delete(PRODUCT_PORT+`/sports/${id}`)
--- src/router/SportsPages/SportsList.js
+++ src/router/SportsPages/SportsList.js
@@ -1,13 +1,14 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import SportsItem from './SportsItem'
-
+import { useDispatch } from 'react-redux'
+import { callSports } from '../../_actions/sports_action'
 export default function SportsList() {
 
-    const [sports, setsports] = useState([
-        {name:"soccer",image:""},
-        {name:"baseball",image:""},
-        {name:"basketball",image:""},
-    ])
+    const [sports, setsports] = useState([])
+    const dispatch = useDispatch()
+    useEffect(()=>{
+        dispatch(callSports).payload.then(res=>setsports(res.data))
+    },[])
 
     return (
         <ul>
